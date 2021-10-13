8fe10d5835c68474145f1770083f3e6d230ff341
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-10-13 20:42:00 -0300

    Home page added

--- React-practice-2/src/routes/App.jsx
+++ React-practice-2/src/routes/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { NotFound } from "../pages/NotFound";
 import { Layout } from "../containers/Layout";
 import { Login } from "../containers/Login";
 import { RecoveryPassword } from "../containers/RecoveryPassword";
@@ -9,14 +11,14 @@ import "../styles/index.scss";
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Layout>
+      <Layout>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/recovery-password" component={RecoveryPassword} />
-          <Route component={NotFound} />
-        </Layout>
-      </Switch>
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
