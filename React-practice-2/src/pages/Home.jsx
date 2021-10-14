91ec25a92b31a935772a3e7d2388bbaf55b1cb09
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-10-14 15:56:41 -0300

    Components done

--- React-practice-2/src/pages/Home.jsx
+++ React-practice-2/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Header } from "../components/Header";
+import { ProductList } from "../containers/ProductList";
 
-function Home() {
-  return <Header />;
-}
+const Home = () => {
+  return (
+    <>
+      <ProductList />
+    </>
+  );
+};
 
 export { Home };
