d4c621e659ce5670aed33b8a016c4d20987a4bb0
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-14 12:59:01 -0300

    migrate to mysql

--- NODEjs-PostgreSQL/config/config.js
+++ NODEjs-PostgreSQL/config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER || 'nico',
+  dbUser: process.env.DB_USER || 'root',
   dbPassword: process.env.DB_PASSWORD || 'admin123',
   dbHost: process.env.DB_HOST || 'localhost',
-  dbPort: process.env.DB_PORT || 5432,
   dbName: process.env.DB_NAME || 'my_store',
+  dbPort: process.env.DB_PORT || 3306,
 };
 
 module.exports = { config };
