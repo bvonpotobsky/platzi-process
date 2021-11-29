e6db3bb4b1bd783117e25221b12990ee1b05f1f2
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-29 05:01:19 -0300

    still some more work to do

--- NODEjs-PostgreSQL/routes/orders.router.js
+++ NODEjs-PostgreSQL/routes/orders.router.js
@@ -1,9 +1,42 @@
 const express = require('express');
 
-const router = express.Router();
+const OrderService = require('../services/order.service');
+const validatorHandler = require('../middlewares/validator.handler');
 
-router.get('/', (req, res) => {
-  res.json([]);
-});
+const {
+  getOrderSchema,
+  createOrderSchema,
+} = require('../schemas/order.schema');
+
+const router = express.Router();
+const service = new OrderService();
+
+router.get(
+  '/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const order = service.findOne(id);
+      return order;
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  '/',
+  validatorHandler(createOrderSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newOrder = await service.create(body);
+      res.status(201).json(newOrder);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
