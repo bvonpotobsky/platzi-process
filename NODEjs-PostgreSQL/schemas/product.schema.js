ec483c5b444c5165fb6a655ff8c7fbee2eac8aa0
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-12-02 23:28:15 -0300

    limit and offset for pagination added

--- NODEjs-PostgreSQL/schemas/product.schema.js
+++ NODEjs-PostgreSQL/schemas/product.schema.js
@@ -6,6 +6,8 @@ const price = Joi.number().integer().min(10);
 const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
+const limit = Joi.number().integer();
+const offset = Joi.number().integer();
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -27,4 +29,14 @@ const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema };
+const queryProductSchema = Joi.object({
+  limit,
+  offset,
+});
+
+module.exports = {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+  queryProductSchema,
+};
