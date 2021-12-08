f3ddc11f50548bb171650fe2abccc73bd8ad9da7
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-12-07 23:29:20 -0300

    this time i fixed it

--- NODEjs-JWT-Auth/config/config.js
+++ NODEjs-JWT-Auth/config/config.js
@@ -5,6 +5,8 @@ const config = {
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   dbUrl: process.env.DATABASE_URL,
+  apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
 }
 
 module.exports = { config };
