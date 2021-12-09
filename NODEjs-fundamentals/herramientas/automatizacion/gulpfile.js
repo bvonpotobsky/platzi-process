93279160cfbd6f222914a29eb4822f85c0de3a43
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-12-09 15:26:41 -0300

    add features

---
+++ NODEjs-fundamentals/herramientas/automatizacion/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const server = require("gulp-server-livereload");
+
+gulp.task("build", function (cb) {
+  console.log("Building site...");
+
+  setTimeout(cb, 1200);
+});
+
+gulp.task("serve", (cb) => {
+  gulp.src("www").pipe(
+    server({
+      livereload: true,
+      open: true,
+    })
+  );
+});
+
+gulp.task("default", gulp.series("build", "serve"));
---
+++ NODEjs-fundamentals/herramientas/electron/index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.on("ready", createWindow);
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
