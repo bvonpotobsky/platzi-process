1691bad143cfbacd11143c14b806a3ea913a88f5
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2022-01-06 17:16:21 -0300

    project

--- WebComponents/ShadowDOM/my-element.js
+++ WebComponents/ShadowDOM/my-element.js
@@ -1,60 +1,35 @@
-const template = document.createElement("div");
-
 class myElement extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
   }
-
   getTemplate() {
     const template = document.createElement("template");
     template.innerHTML = `
       <section>
-        <h2>Titulo creado desde el template</h2>
+        <h2 class="title">Hola mundo!</h2>
         <div>
-          <h1>
-            <slot></slot>
-          </h1>
-          <h3>Hola Mundo!</h3>
-          <p>Soy un parrafo</p>
+          <p>Soy más texto ejemplo</p>
         </div>
       </section>
       ${this.getStyles()}
     `;
-
     return template;
   }
-
   getStyles() {
     return `
       <style>
-        section {
-          position: relative;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          border: 1px solid red;
-        }
-
         h2 {
           color: red;
-        }
-
-        section div {
-          color: blue;
         }
       </style>
     `;
   }
-
   render() {
     this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
   }
-
   connectedCallback() {
     this.render();
   }
 }
-
 customElements.define("my-element", myElement);
