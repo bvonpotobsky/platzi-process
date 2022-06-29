faa3d6ffa91712fb77328e334c59b738a9953092
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2022-06-28 21:38:29 -0300

    more typescript

--- Typescript-fundamentals/src/hla.ts
+++
@@ -1,5 +0,0 @@
-export const person = {
-  name: "John",
-};
-
-person;
--- Typescript-fundamentals/src/typing.ts
+++ Typescript-fundamentals/src/typing.ts
@@ -1,13 +1,80 @@
-export let productName: string = "Product";
+(() => {
+  // Type
+  type UserID = string | number | boolean;
 
-export let productPrice: number = 100;
+  function greeting(userID: UserID) {
+    if (typeof userID === "string") {
+      return `string: ${userID.toLowerCase()}`;
+    }
 
-export const otherPrice: number = 200;
+    if (typeof userID === "number") {
+      return `number: ${userID.toFixed()}`;
+    }
 
-export const total: number = productPrice + otherPrice;
+    if (typeof userID === "boolean") {
+      return `boolean: ${userID}`;
+    }
+  }
 
-const addNumber = (a: number, b: number): number => {
-  return a + b;
-};
+  greeting("hola");
+  greeting(2);
+  greeting(true);
 
-addNumber(1, 2);
+  // Literal types
+  type Sizes = "XS" | "S" | "M" | "L" | "XL";
+
+  // Null !== undefined
+  let myNumber: number;
+  let myString: string;
+
+  const createProductToJson = (
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size?: Sizes
+  ) => {
+    return {
+      title,
+      createdAt,
+      stock,
+      size,
+    };
+  };
+  const productOne = createProductToJson("p1", new Date(), 12);
+
+  const calcTotal = (prices: number[]): number => {
+    let total: number = 0;
+    prices.forEach((price) => (total += price));
+
+    return total;
+  };
+
+  const getTotal = calcTotal([1, 2, 3, 4]);
+
+  // Object and functions
+
+  const login = (data: { email: string; password: string }) => {
+    console.log(data.email, data.password);
+  };
+
+  login({
+    email: "bvp@gmail.com",
+    password: "1234",
+  });
+
+  const products: any[] = [];
+  const addProduct = (data: {
+    title: string;
+    createdAt: Date;
+    stock: number;
+    size?: Sizes;
+  }) => {
+    products.push(data);
+  };
+
+  addProduct({
+    title: "remera",
+    createdAt: new Date(),
+    stock: 12,
+  });
+})();
